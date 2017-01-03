a2a7f236315329dba52423b52153998e09b229f1
Graham84 <gosiochain@rocketmail.com>
2017-01-03 01:21:47 +0000

    Chapter 5

---
+++ Chapter 5 - Objects and Object-Orientation/import module vs  from module import.py
@@ -0,0 +1,16 @@
+"""
+#>>> import types
+#>>> types.FunctionType
+<type 'function'>
+#>>> FunctionType
+
+Traceback (most recent call last):
+  File "<pyshell#153>", line 1, in <module>
+    FunctionType
+NameError: name 'FunctionType' is not defined
+#>>> from types import FunctionType
+#>>> FunctionType
+<type 'function'>
+#>>>
+
+"""
