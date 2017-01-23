7e16a14620ca0c8afd2c69de1b948b7117e6c811
Graham84 <gosiochain@rocketmail.com>
2017-01-23 00:34:12 +0000

    Chapter 9 - XML Processing - 23/01/17

---
+++ Chapter 9 - XML Processing/PackagesAreModulesToo.py
@@ -0,0 +1,14 @@
+from xml.dom import minidom
+print minidom
+
+print minidom.Element
+
+from xml.dom.minidom import Element
+print Element
+print minidom.Element
+
+from xml import dom
+print dom
+
+import xml
+print xml
