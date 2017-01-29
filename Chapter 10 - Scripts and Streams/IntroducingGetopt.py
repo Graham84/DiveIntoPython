85a84d4ab966fec94279991f7324068ee27bfe27
Graham84 <gosiochain@rocketmail.com>
2017-01-29 16:58:40 +0000

    Chapter 10 Scripts and Streams - Chapter End 29-01-17

---
+++ Chapter 10 - Scripts and Streams/IntroducingGetopt.py
@@ -0,0 +1,12 @@
+# def main(argv):
+#     grammar = "kant.xml"
+#     try:
+#         opts, args = getopt.getopt.getopt(argv, "hg:d", ["help", "grammar="])
+#     except getopt.GetoptError:
+#         usage()
+#         sys.exit(2)
+
+# ...
+
+# if __name__ == "__main__":
+#     main(sys.argv[1:])
