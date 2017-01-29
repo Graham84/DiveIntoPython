54bde5f6a85de5cce73f024376f949f31ff00fa1
Graham84 <gosiochain@rocketmail.com>
2017-01-29 17:58:55 +0000

    Chapter 11 HTTP Web Services 29-01-17

---
+++ Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
@@ -0,0 +1,5 @@
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
