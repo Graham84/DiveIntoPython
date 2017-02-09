b4ceb0d2d7ddf454e6090c35c9641c70e86c26da
Graham84 <gosiochain@rocketmail.com>
2017-02-09 20:15:31 +0000

    Chapter 11 HTTP Web Services 09-02-17

--- Chapter 11 - HTTP Web Services/SupportingEtagIfNoneMatch.py
+++ Chapter 11 - HTTP Web Services/SupportingEtagIfNoneMatch.py
@@ -8,13 +8,3 @@ firstdata = firstdatastream.read()
 print firstdata
 request.add_header()
 
-
-import urllib2, openanything
-request = urllib.Request('http://diveintomark.org/xml/atom.xml')
-opener = urllib2.build_opener(
-    openanything.DefaultErrorhandler())
-firstdatastream = opener.open(request)
-firstdatastream.headers.get('ETag')
-firstdata = firstdatastream.read()
-print firstdata
-request.add_header()
