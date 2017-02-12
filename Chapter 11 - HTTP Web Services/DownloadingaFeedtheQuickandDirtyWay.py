7f939cd8a58389c314018d9fa54c8e44e26d05e6
Graham84 <gosiochain@rocketmail.com>
2017-02-12 18:49:47 +0000

    12-02-17

--- Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
+++ Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
@@ -10,12 +10,10 @@ import urllib
 data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print data
 
-
 # how not to fetch data over HTTP
 import urllib
 # data = urllib.urlopen('http://google.com').read()
 data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print data
 
-
-786
+789
