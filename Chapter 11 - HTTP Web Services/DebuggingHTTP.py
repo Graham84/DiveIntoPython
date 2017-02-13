55995d1e2f641b43b48c594ee743aa12a450b473
Graham84 <gosiochain@rocketmail.com>
2017-02-13 19:03:16 +0000

    13-02-17

--- Chapter 11 - HTTP Web Services/DebuggingHTTP.py
+++ Chapter 11 - HTTP Web Services/DebuggingHTTP.py
@@ -3,3 +3,17 @@ httplib.HTTPConnection.debuglevel = 1
 import urllib
 feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print feeddata
+
+import httplib
+httplib.HTTPConnection.debuglevel = 1
+import urllib
+feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print feeddata
+
+import httplib
+httplib.HTTPConnection.debuglevel = 1
+import urllib
+feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print feeddata
+
+4789
