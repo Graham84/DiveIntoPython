8a3140ed176aa15d8ba93dd4c02df260ca5e5bc9
Graham84 <gosiochain@rocketmail.com>
2017-02-15 20:18:29 +0000

    15-02-17

--- Chapter 11 - HTTP Web Services/DebuggingHTTP.py
+++ Chapter 11 - HTTP Web Services/DebuggingHTTP.py
@@ -3,3 +3,25 @@ httplib.HTTPConnection.debuglevel = 1
 import urllib
 feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print feeddata
+
+People Just Do Nothing
+
+
+import httplib
+httplib.HTTPConnection.debuglevel = 1
+import urllib
+feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print feeddata
+
+People Just Do Nothing
+
+
+import httplib
+httplib.HTTPConnection.debuglevel = 1
+import urllib
+feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print feeddata
+
+People Just Do Nothing
+
+85
--- Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
+++ Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
@@ -3,17 +3,3 @@ import urllib
 # data = urllib.urlopen('http://google.com').read()
 data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print data
-
-# how not to fetch data over HTTP
-import urllib
-# data = urllib.urlopen('http://google.com').read()
-data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print data
-
-# how not to fetch data over HTTP
-import urllib
-# data = urllib.urlopen('http://google.com').read()
-data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print data
-
-789
