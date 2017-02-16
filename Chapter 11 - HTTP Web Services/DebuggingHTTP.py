3d20c1b322196afd16bc8a367ef1b247e451464e
Graham84 <gosiochain@rocketmail.com>
2017-02-16 19:06:45 +0000

    16-02-17

--- Chapter 11 - HTTP Web Services/DebuggingHTTP.py
+++ Chapter 11 - HTTP Web Services/DebuggingHTTP.py
@@ -4,24 +4,3 @@ import urllib
 feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print feeddata
 
-People Just Do Nothing
-
-
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-
-People Just Do Nothing
-
-
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-
-People Just Do Nothing
-
-85
--- Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
+++ Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
@@ -3,3 +3,18 @@ import urllib
 # data = urllib.urlopen('http://google.com').read()
 data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print data
+
+Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
+
+Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
+
+
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
+
+Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
+
+Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
