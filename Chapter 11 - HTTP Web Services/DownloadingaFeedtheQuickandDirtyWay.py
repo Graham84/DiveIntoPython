21f844f840dda73dbb6cf1cc438c0f73a15645fa
Graham84 <gosiochain@rocketmail.com>
2017-02-17 18:38:16 +0000

    17-02-17

--- Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
+++ Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
@@ -4,10 +4,11 @@ import urllib
 data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print data
 
-Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
-
-Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
-
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
 
 # how not to fetch data over HTTP
 import urllib
@@ -15,6 +16,9 @@ import urllib
 data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print data
 
-Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
-
-Sony MDRZX220BTH CE7 Wireless On-Ear Headphones - Blue
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
+456
