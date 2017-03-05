bdf9f7a8b03ad672487e5afe8d3a157e7a6197cc
Graham84 <gosiochain@rocketmail.com>
2017-03-05 09:47:05 +0000

    05-03-17

--- Chapter 11 - HTTP Web Services/DebuggingHTTP.py
+++ Chapter 11 - HTTP Web Services/DebuggingHTTP.py
@@ -3,33 +3,4 @@ httplib.HTTPConnection.debuglevel = 1
 import urllib
 feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print feeddata
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
+
--- Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
+++ Chapter 11 - HTTP Web Services/DownloadingaFeedtheQuickandDirtyWay.py
@@ -3,3 +3,33 @@ import urllib
 # data = urllib.urlopen('http://google.com').read()
 data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print data
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
+# how not to fetch data over HTTP
+import urllib
+# data = urllib.urlopen('http://google.com').read()
+data = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
+print data
