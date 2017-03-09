37fd96818b5169c9a4e4a217a1bd1cb2aafadf12
Graham84 <gosiochain@rocketmail.com>
2017-03-09 17:49:41 +0000

    09/03/17 commit

--- Chapter 11 - HTTP Web Services/DebuggingHTTP.py
+++ Chapter 11 - HTTP Web Services/DebuggingHTTP.py
@@ -4,33 +4,3 @@ import urllib
 feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
 print feeddata
 
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-
-import httplib
-httplib.HTTPConnection.debuglevel = 1
-import urllib
-feeddata = urllib.urlopen('http://diveintomark.org/xml/atom.xml').read()
-print feeddata
-
--- Chapter 11 - HTTP Web Services/IntroducingUrllib2.py
+++ Chapter 11 - HTTP Web Services/IntroducingUrllib2.py
@@ -11,4 +11,43 @@ print request
 print request.get_full_url()
 print request.add_header('User-Agent', 'OpenAnything/1.0 +http://diveintopython.org/')
 feeddata = opener.open(request).read()
+print feeddataimport httplib
+httplib.HTTPConnection.debuglevel = 1
+import urllib2
+request = urllib2.Request('http://diveintomark.org/xml/atom.xml')
+opener = urllib2.build_opener()
+feeddata = opener.open(request).read()
+print feeddata
+
+# adding Headers with the Request Object
+print request
+print request.get_full_url()
+print request.add_header('User-Agent', 'OpenAnything/1.0 +http://diveintopython.org/')
+feeddata = opener.open(request).read()
+print feeddataimport httplib
+httplib.HTTPConnection.debuglevel = 1
+import urllib2
+request = urllib2.Request('http://diveintomark.org/xml/atom.xml')
+opener = urllib2.build_opener()
+feeddata = opener.open(request).read()
+print feeddata
+
+# adding Headers with the Request Object
+print request
+print request.get_full_url()
+print request.add_header('User-Agent', 'OpenAnything/1.0 +http://diveintopython.org/')
+feeddata = opener.open(request).read()
+print feeddataimport httplib
+httplib.HTTPConnection.debuglevel = 1
+import urllib2
+request = urllib2.Request('http://diveintomark.org/xml/atom.xml')
+opener = urllib2.build_opener()
+feeddata = opener.open(request).read()
+print feeddata
+
+# adding Headers with the Request Object
+print request
+print request.get_full_url()
+print request.add_header('User-Agent', 'OpenAnything/1.0 +http://diveintopython.org/')
+feeddata = opener.open(request).read()
 print feeddata
