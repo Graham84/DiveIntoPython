896186673d7c3ff82de19e428724ebd7ec809f6d
Graham84 <gosiochain@rocketmail.com>
2017-03-18 10:22:12 +0000

    18/03/17

--- Chapter 11 - HTTP Web Services/DefiningaURLHandler.py
+++ Chapter 11 - HTTP Web Services/DefiningaURLHandler.py
@@ -8,3 +8,103 @@ class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
             req.get_full_url(), code,msg,headers,fp)
         result.status = code
         return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
+import urllib2
+
+
+class DefaultErrorHandler(urllib2.HTTPDefaultErrorHandler):
+
+    def http_error_default(self, req, fp, code, msg, headers):
+        result = urllib2.HTTPError(
+            req.get_full_url(), code,msg,headers,fp)
+        result.status = code
+        return result
