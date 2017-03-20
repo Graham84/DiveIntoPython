2d6cba8e333b44675605f7c0229867e52e3be6fd
Graham84 <gosiochain@rocketmail.com>
2017-03-20 19:01:47 +0000

    20/03/17

--- Chapter 11 - HTTP Web Services/TestingLastModiffied.py
+++ Chapter 11 - HTTP Web Services/TestingLastModiffied.py
@@ -3,33 +3,3 @@ request = urllib2.Request('http://diveintomatk.org/xml/atom.xml')
 opener = urllib2.build_opener()
 firstdatastream = opener.open(request)
 print firstdatastream.headers.dict
-import urllib2
-request = urllib2.Request('http://diveintomatk.org/xml/atom.xml')
-opener = urllib2.build_opener()
-firstdatastream = opener.open(request)
-print firstdatastream.headers.dict
-import urllib2
-request = urllib2.Request('http://diveintomatk.org/xml/atom.xml')
-opener = urllib2.build_opener()
-firstdatastream = opener.open(request)
-print firstdatastream.headers.dict
-import urllib2
-request = urllib2.Request('http://diveintomatk.org/xml/atom.xml')
-opener = urllib2.build_opener()
-firstdatastream = opener.open(request)
-print firstdatastream.headers.dict
-import urllib2
-request = urllib2.Request('http://diveintomatk.org/xml/atom.xml')
-opener = urllib2.build_opener()
-firstdatastream = opener.open(request)
-print firstdatastream.headers.dict
-import urllib2
-request = urllib2.Request('http://diveintomatk.org/xml/atom.xml')
-opener = urllib2.build_opener()
-firstdatastream = opener.open(request)
-print firstdatastream.headers.dict
-import urllib2
-request = urllib2.Request('http://diveintomatk.org/xml/atom.xml')
-opener = urllib2.build_opener()
-firstdatastream = opener.open(request)
-print firstdatastream.headers.dict
