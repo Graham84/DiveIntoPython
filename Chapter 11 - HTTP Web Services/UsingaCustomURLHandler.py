5f7203e56d4a0da16be7e7f5598a67883fcc741c
Graham84 <gosiochain@rocketmail.com>
2017-03-23 21:25:00 +0000

    23/03/17

--- Chapter 11 - HTTP Web Services/UsingaCustomURLHandler.py
+++ Chapter 11 - HTTP Web Services/UsingaCustomURLHandler.py
@@ -5,3 +5,31 @@ opener = urllib2.build_opener(
 seconddatastream = oener.open(request)
 print seconddatastream.status
 print seconddatastream.read()
+request.eaders
+import openanything
+opener = urllib2.build_opener(
+    openanything.DefaultErrorHandler())
+seconddatastream = oener.open(request)
+print seconddatastream.status
+print seconddatastream.read()
+request.eaders
+import openanything
+opener = urllib2.build_opener(
+    openanything.DefaultErrorHandler())
+seconddatastream = oener.open(request)
+print seconddatastream.status
+print seconddatastream.read()
+request.eaders
+import openanything
+opener = urllib2.build_opener(
+    openanything.DefaultErrorHandler())
+seconddatastream = oener.open(request)
+print seconddatastream.status
+print seconddatastream.read()
+request.eaders
+import openanything
+opener = urllib2.build_opener(
+    openanything.DefaultErrorHandler())
+seconddatastream = oener.open(request)
+print seconddatastream.status
+print seconddatastream.read()
